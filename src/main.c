5b8e20065adf258657212c01dae44d9da687fcc0
kapell <kasperi.apell@gmail.com>
2024-09-22 17:11:01 +0300

    Add a server module

--- src/main.c
+++ src/main.c
@@ -1,85 +1,64 @@
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <time.h>
 #include <unistd.h>
 
+#include "server.h"
 
-const int MAX_CLIENTS = 128;
-const int ID_SZ = 16;
-const int THROTTLE_LAG = 1;
+
+#define PORT 5000
+#define QUEUE_LENGTH 16
+#define THROTTLE_LAG 1
+
 const socklen_t SOCKADDR_SZ = sizeof(struct sockaddr_in);
+int prev_id = -1;
 
-typedef struct {
-        char id[ID_SZ]; /* ID displayed next to the messages */
-        int fd;
-        struct sockaddr_in addr;
-} Client;
-
-Client *clients[MAX_CLIENTS];
-
-
-/* Add a client to the list of clients.
-   First confirm that the client ID is not already taken. */
-int
-add(Client *client)
-{
-        int avail_idx = -1;
-        for (int i=0; i<MAX_CLIENTS; i++) {
-                if (!clients[i] && !strmp(clients[i]->id, client->id))
-                        return 0; /* ID is already in use */
-                if (avail_idx<0 && !clients[i])
-                        avail_idx = i;
-        }
-        if (avail_idx > 0) {
-                clients[avail_idx] = client;
-                return 1;
-        }
-        return 0;
-}
-
-int
-remove(char id[])
-{
-        for (int i=0; i<MAX_CLIENTS; i++)
-                if (clients[i] && !strcmp(clients[i]->id, id)) {
-                        clients[i] = 0;
-                        return 1;
-                }
-        return 0;
-}
-
-/* Broadcast a message to every client */
-void
-sendall(char *s)
-{
-        int nbytes = 0;
-        for (int i=0; i<MAX_CLIENTS; i++)
-                if (clients[i]) {
-                        nbytes = write(clients[i]->fd, s, strlen(s)+1);
-                        if (nbytes < 0)
-                                exit(EXIT_FAILURE);
-                }
-}
 
 int
 main(int argc, char *argv[])
 {
         int clifd = 0, servfd = 0, clicnt = 0;
         struct sockaddr_in cliaddr, servaddr;
+        pthread_t tid;
 
         servfd = socket(AF_INET, SOCK_STREAM, 0);
         if (servfd < 0)
                 return EXIT_FAILURE; /* Socket creation failed */
 
         servaddr.sin_family = AF_INET;
-        servaddr.sin_addr.s_addr = INADDR_ANY;
-        servaddr.sin_port = 2048;
+        servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        servaddr.sin_port = htons(PORT);
+
+        if (bind(servfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+                perror("Failed to bind server socket");
+                return EXIT_FAILURE;
+        }
+
+        if (listen(servfd, QUEUE_LENGTH) < 0) {
+                perror("Socket listening failed");
+                return EXIT_FAILURE;
+        }
 
         while (1) {
                 if (clicnt == MAX_CLIENTS)
                         continue;
-                clifd = accept(servfd, (struct sockaddr*)&cliaddr, (socklen_t*)&SOCKADDR_SZ);
+                socklen_t clisz = sizeof(cliaddr);
+                clifd = accept(servfd, (struct sockaddr*)&cliaddr, (socklen_t*)&clisz);
+                if (clifd < 0) {
+                        perror("Failed to accept new client connection");
+                        return EXIT_FAILURE;
+                }
+                Client *client = (Client *) malloc(sizeof(Client));
+                client->addr = cliaddr;
+                client->fd = clifd;
+                client->id = ++prev_id;
+                clicnt++;
+
+                add_client(client);
+                pthread_create(&tid, NULL, &client_interface, (void*)client);
+
                 sleep(THROTTLE_LAG);
         }
         return EXIT_SUCCESS;
